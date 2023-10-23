b97d156b8a14b108180f66f61f5aa0f8ed7159bb
Alan Shipman <shipmaal@bc.ed>
2023-10-23 11:49:33 -0400

    Added data extraction to project repo

--- dataExtraction.py
+++ dataExtraction.py
@@ -1,15 +1,16 @@
+import numpy as np
 import xarray as xr
-import pandas as pd
 
-filepath = '/home/shipmaal/SWOT-ML/MissouriUpstream.nc'
+filepath = '/home/shipmaal/SWOT-ML/MissouriUpstream.nc' # replace with filepath of the data file
 
 with xr.open_dataset(filepath, group='Reach_Timeseries') as ds:
-    print(ds.variables)
-    width = ds.W
-    discharge = ds.H
-    flow_area = ds.P
-    surface_elevation = ds.S
-    surface_slop = ds.A
+    width = np.array(ds.W)
+    discharge = np.array(ds.Q)
+    average_area = np.array(ds.A)
+    surface_elevation = np.array(ds.H)
+    surface_slop = np.array(ds.S)
 
+dataset = np.stack((width, discharge, average_area, surface_elevation, surface_slop), axis=1)
 
-
+print(dataset)
+print(dataset.shape)
